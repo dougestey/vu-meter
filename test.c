a299fee47730587ff0d56aaeab70821b7adff442
Doug Estey <doug@designcofounders.com>
2015-10-31 10:46:32 -0400

    Update test.c

--- test.c
+++ test.c
@@ -4,9 +4,8 @@ int barThree = 4;
 int barFour = 5;
 int barFive = 6;
 int barSix = 7;
-int barSevenR = 8;
-int barSevenG = 9;
-int barEight = 10;
+int barSeven = 8;
+int barEight = 9;
 
 void setup()
 {
@@ -16,8 +15,7 @@ void setup()
   pinMode(barFour, OUTPUT);
   pinMode(barFive, OUTPUT);
   pinMode(barSix, OUTPUT);
-  pinMode(barSevenR, OUTPUT);
-  pinMode(barSevenG, OUTPUT);
+  pinMode(barSeven, OUTPUT);
   pinMode(barEight, OUTPUT);
 }
 
@@ -35,14 +33,7 @@ void loop()
   delay(750);
   digitalWrite(barSix, HIGH);
   delay(1000);
-  for (int i = 0; i < 355; i++){
-    digitalWrite(barSevenG, LOW);
-    digitalWrite(barSevenR, HIGH);
-    delay(5);
-    digitalWrite(barSevenR, LOW);
-    digitalWrite(barSevenG, HIGH);
-    delay(5);
-  }
+  digitalWrite(barSeven, HIGH);
   delay(1500);
   digitalWrite(barEight, HIGH);
   delay(2000);
@@ -50,8 +41,7 @@ void loop()
   // lows
   digitalWrite(barEight, LOW);
   delay(50);
-  digitalWrite(barSevenG, LOW);
-  digitalWrite(barSevenR, LOW);
+  digitalWrite(barSeven, LOW);
   delay(50);
   digitalWrite(barSix, LOW);
   delay(50);
